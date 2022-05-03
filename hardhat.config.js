6126dad066565a3628595ba696f9c31754bf8ae2
fanxiushan <fanxiushan@gmail.com>
2022-05-03 23:05:02 +0800

    向 ropsten 部署测试合约
    Signed-off-by: fanxiushan <fanxiushan@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,22 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+
 require("@nomiclabs/hardhat-ethers")
+require('dotenv').config();
+
+task("accounts", "print account").setAction(async() => {
+  console.log(process.env.PRIVATE_KEY_0)
+  console.log(process.env.PUBLIC_KEY_0)
+})
+
 
 module.exports = {
   solidity: "0.8.8",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/woaYI6yThUOVdkyhinzxu34dF5hTmdfj`,
+      accounts: [`${process.env.PRIVATE_KEY_0}`]
+    }
+  }
 };
